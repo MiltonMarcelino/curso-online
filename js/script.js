da542b808b5e7213f3ccae5c244f40f58708a10d
smarcelino.mj@gmail.com <smarcelino.mj@gmail.com>
2025-06-12 16:05:34 -0300

    Manipulação, estilização de janela modal.

--- js/script.js
+++ js/script.js
@@ -39,10 +39,27 @@
 
 // DOM - Document Object Model
 
-let botao = document.getElementById("btnAbrir");
+//let botao = document.getElementById("btnAbrir");
 
-botao.addEventListener("click", function() {
-    alert("Olá mundo aqui de dentro do JS..."); 
+//botao.addEventListener("click", function() {
+//    alert("Olá mundo aqui de dentro do JS..."); 
+//});
+
+let boxModal = document.getElementById("meuModal");
+
+//boxModal.showModal();
+
+let linkjs = document.getElementById("linkJs");
+let linkHtml = document.getElementById("linkHtml");
+let linkCss = document.getElementById("linkCss");
+
+linkjs.addEventListener("click", function(){
+boxModal.showModal();
+
+//Inserindo o texto do curso no modal via innerHTML.
+//Inserindo o texto do curso no modal via innerHTML.
+  boxModal.innerHTML += "<div><p>JavaScript (frequentemente abreviado como JS) é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e funcional).[2][3] Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.</p><p>JavaScript permite páginas da Web interativas e, portanto, é uma parte essencial dos aplicativos da web. A grande maioria dos sites usa, e todos os principais navegadores têm um mecanismo JavaScript dedicado para executá-lo.[4] É atualmente a principal linguagem para programação client-side em navegadores web. É também bastante utilizada do lado do servidor através de ambientes como o node.js.</p></div>"
+ 
 });
 
 
