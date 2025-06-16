d1d38443fc3cb1af3c0e9a85a7b683e4083f6043
smarcelino.mj@gmail.com <smarcelino.mj@gmail.com>
2025-06-16 16:08:47 -0300

    Finalizando o modal com manipulação do DOM.

--- js/script.js
+++ js/script.js
@@ -53,13 +53,47 @@ let linkjs = document.getElementById("linkJs");
 let linkHtml = document.getElementById("linkHtml");
 let linkCss = document.getElementById("linkCss");
 
-linkjs.addEventListener("click", function(){
+linkjs.addEventListener("click", function(e){
+  e.preventDefault();
 boxModal.showModal();
 
 //Inserindo o texto do curso no modal via innerHTML.
 //Inserindo o texto do curso no modal via innerHTML.
-  boxModal.innerHTML += "<div><p>JavaScript (frequentemente abreviado como JS) é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e funcional).[2][3] Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.</p><p>JavaScript permite páginas da Web interativas e, portanto, é uma parte essencial dos aplicativos da web. A grande maioria dos sites usa, e todos os principais navegadores têm um mecanismo JavaScript dedicado para executá-lo.[4] É atualmente a principal linguagem para programação client-side em navegadores web. É também bastante utilizada do lado do servidor através de ambientes como o node.js.</p></div>"
+let boxInner = document.getElementById("box");
+
+boxInner.innerHTML = "";
+  boxInner.innerHTML += "<div><p>JavaScript (frequentemente abreviado como JS) é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e funcional).[2][3] Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.</p><p>JavaScript permite páginas da Web interativas e, portanto, é uma parte essencial dos aplicativos da web. A grande maioria dos sites usa, e todos os principais navegadores têm um mecanismo JavaScript dedicado para executá-lo.[4] É atualmente a principal linguagem para programação client-side em navegadores web. É também bastante utilizada do lado do servidor através de ambientes como o node.js.</p></div>"
  
+});
+
+linkHtml.addEventListener("click", function(e){
+  e.preventDefault();
+boxModal.showModal();
+let boxInner = document.getElementById("box");
+boxInner.innerHTML = "";
+boxInner.innerHTML += "<div<p>Criada pelo britânico Tim Berners-Lee, o acrônimo HTML significa HiperText Markup Language, traduzindo ao português: Linguagem de Marcação de Hipertexto. O HTML é o componente básico da web, ele permite inserir o conteúdo e estabelecer a estrutura básica de um website.</p></div>"
+});
+
+
+linkCss.addEventListener("click", function(e){
+  e.preventDefault();
+boxModal.showModal();
+let boxInner = document.getElementById("box");
+boxInner.innerHTML = "";
+boxInner.innerHTML += "<div<p>O CSS foi desenvolvido pelo W3C (World Wide Web Consortium) em 1996 por um motivo bastante simples: a linguagem HTML não foi projetada para ter tags que ajudassem a formatar a página — o objetivo era apenas escrever a marcação para o conteúdo da web.Tags como <font> foram introduzidas na versão 3.2 do HTML e causaram muitos problemas para os desenvolvedores. Devido ao fato de as páginas da web terem fontes diferentes, fundos coloridos e vários estilos, reescrever o código era um processo longo, trabalhoso e caro. Desta forma, o CSS foi criado pelo W3C para resolver esse problema.</p></div>"
+});
+
+
+
+
+
+
+/// Capturando o botão de fechar do Modal!
+
+let botaoFecharModal = document.querySelector('.close');
+
+botaoFecharModal.addEventListener("click", function(e){
+  boxModal.close();
 });
 
 
